d087388a73e36767b77f32f8eb6341517024e009
nihara <nbmayurawasala@gmail.com>
2022-12-03 13:39:49 +0530

    initial commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Item",
+    description: "This is the description",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Item",
+    description: "This is the description2",
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
