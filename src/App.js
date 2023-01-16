41f1754c5d9ea52aa0b46c9d1dc8578c2d7812f9
nihara <nbmayurawasala@gmail.com>
2023-01-16 17:32:32 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import Notification from './components/UI/Notification';
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
-import { uiActions } from "./store/ui-slice";
-
-let isInitial = true; 
+let isInitial = true;
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
@@ -17,55 +16,28 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cat data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-demo-10627-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
+    dispatch(fetchCartData());
+  }, dispatch);
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success...",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    if(isInitial){
-      // To avoid send blank cart data when the useEffect run for the first time
-      isInitial=false; 
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
     <>
-    {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>} 
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Layout>
         {cartIsVisible && <Cart />}
         <Products />
